444cf5a85a42a8d51eeb73e9a89d15fadbca749e
Bryce Roethel <me@bryceroethel.com>
2026-02-18 15:25:41 -0500

    Modify multi-target pipe to filter for pairs and remove unused workflows

--- photon-core/src/main/java/org/photonvision/vision/pipe/impl/MultiTargetPNPPipe.java
+++ photon-core/src/main/java/org/photonvision/vision/pipe/impl/MultiTargetPNPPipe.java
@@ -18,8 +18,10 @@
 package org.photonvision.vision.pipe.impl;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.photonvision.common.logging.LogGroup;
 import org.photonvision.common.logging.Logger;
@@ -37,6 +39,36 @@ public class MultiTargetPNPPipe
                 Optional<MultiTargetPNPResult>,
                 MultiTargetPNPPipe.MultiTargetPNPPipeParams> {
     private static final Logger logger = new Logger(MultiTargetPNPPipe.class, LogGroup.VisionModule);
+
+    private static final Map<Integer, Integer> pairs = new HashMap<>();
+
+    static {
+        // Hard-coded 2026 REBUILT AprilTag pairs
+        pairs.put(2, 11);
+        pairs.put(11, 2);
+        pairs.put(3, 4);
+        pairs.put(4, 3);
+        pairs.put(5, 8);
+        pairs.put(8, 5);
+        pairs.put(9, 10);
+        pairs.put(10, 9);
+        pairs.put(13, 14);
+        pairs.put(14, 13);
+        pairs.put(15, 16);
+        pairs.put(16, 15);
+        pairs.put(18, 27);
+        pairs.put(27, 18);
+        pairs.put(19, 20);
+        pairs.put(20, 19);
+        pairs.put(21, 24);
+        pairs.put(24, 21);
+        pairs.put(25, 26);
+        pairs.put(26, 25);
+        pairs.put(29, 30);
+        pairs.put(30, 29);
+        pairs.put(31, 32);
+        pairs.put(32, 31);
+    }
 
     private boolean hasWarned = false;
 
@@ -58,31 +90,52 @@ public class MultiTargetPNPPipe
     }
 
     private Optional<MultiTargetPNPResult> calculateCameraInField(List<TrackedTarget> targetList) {
-        // Find tag IDs that exist in the tag layout
-        var tagIDsUsed = new ArrayList<Short>();
-        for (var target : targetList) {
-            int id = target.getFiducialId();
-            if (params.atfl().getTagPose(id).isPresent()) tagIDsUsed.add((short) id);
+        // This method is modified to preemptively filter for AprilTag "pairs", to
+        // ensure multi-target estimates are guaranteed to only include tags that
+        // share the same point of interest, with the goal of preventing noise from
+        // background tags affecting the accuracy of vision estimates.
+
+        // All estimates returned by this method will only include the closest pair
+        // of tags to the camera, with all other tags supplied in the targetList being
+        // left unused.
+
+        var targetArray = (TrackedTarget[]) targetList.toArray();
+        Arrays.sort(
+                targetArray,
+                (a, b) -> {
+                    double a_d2 = a.getBestCameraToTarget3d().getTranslation().getSquaredNorm();
+                    double b_d2 = b.getBestCameraToTarget3d().getTranslation().getSquaredNorm();
+                    return a_d2 == b_d2 ? 0 : a_d2 < b_d2 ? -1 : 1;
+                });
+
+        for (var target : targetArray) {
+            int targetId = target.getFiducialId();
+            Integer pairId = pairs.get(targetId);
+
+            if (pairId != null) {
+                for (var secondary : targetArray) {
+                    if (secondary.getFiducialId() == pairId) {
+                        var estimatedPose =
+                                VisionEstimation.estimateCamPosePNP(
+                                        params.cameraCoefficients().cameraIntrinsics.getAsWpilibMat(),
+                                        params.cameraCoefficients().distCoeffs.getAsWpilibMat(),
+                                        TrackedTarget.simpleFromTrackedTargets(List.of(target, secondary)),
+                                        params.atfl(),
+                                        params.targetModel());
+
+                        if (estimatedPose.isPresent()) {
+                            return Optional.of(
+                                    new MultiTargetPNPResult(
+                                            estimatedPose.get(), List.of((short) targetId, pairId.shortValue())));
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
-        // Only run with multiple targets
-        if (tagIDsUsed.size() < 2) {
-            return Optional.empty();
-        }
-
-        var estimatedPose =
-                VisionEstimation.estimateCamPosePNP(
-                        params.cameraCoefficients().cameraIntrinsics.getAsWpilibMat(),
-                        params.cameraCoefficients().distCoeffs.getAsWpilibMat(),
-                        TrackedTarget.simpleFromTrackedTargets(targetList),
-                        params.atfl(),
-                        params.targetModel());
-
-        if (estimatedPose.isPresent()) {
-            return Optional.of(new MultiTargetPNPResult(estimatedPose.get(), tagIDsUsed));
-        } else {
-            return Optional.empty();
-        }
+        return Optional.empty();
     }
 
     public static record MultiTargetPNPPipeParams(
